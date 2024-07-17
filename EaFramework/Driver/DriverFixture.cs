5afc5d047887e52168f035d938b5cdc795cc9ea7
edwinchi <chipineiro@gmail.com>
2024-07-17 17:50:02 -0600

    install dependencies in new solution, move driver logic to DriverFixture

--- EaFramework/Driver/DriverFixture.cs
+++ EaFramework/Driver/DriverFixture.cs
@@ -1,7 +1,36 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+
 namespace DriverFixture.Driver
 {
     public class DriverFixture
     {
-        
+        public IWebDriver Driver {get;}
+
+        public DriverFixture()
+        {
+            Driver = GetDriverType(BrowserType.Firefox);
+            Driver.Navigate().GoToUrl("https://google.com");
+        }
+
+        private IWebDriver GetDriverType(BrowserType browserType)
+        {
+            return browserType switch
+            {
+                BrowserType.Chrome => new ChromeDriver(),
+                BrowserType.Firefox => new FirefoxDriver(),
+                BrowserType.Edge => new EdgeDriver(),
+                _ => new ChromeDriver()
+            };
+        }
+
+        public enum BrowserType
+        {
+            Chrome,
+            Firefox,
+            Edge
+        }
     }
 }
